b7ec9a5f614810b8c26feaa1e44935634713a245
c1505 <coreym247@gmail.com>
2016-05-14 12:22:52 -0400

    behavior working

--- js/app/directives/Tab.js
+++ js/app/directives/Tab.js
@@ -1,19 +1,32 @@
 function tab() {
   return {
     restrict: 'E',
-    scope: {
+    scope:{
       label: '@'
     },
     require: '^tabs',
     transclude: true, 
-    template: 'I am a tab! {{thing}}',
+    // template: 'I am a tab! {{thing}}',
+    template: [
+      "<li ng-click='tab.toggle()'>{{label}}",
+        "<div ng-transclude ng-show='tab.selected'></div>",
+      "</li>"
+      ].join(""),
+    controller: function() {
+      this.selected = false;
+      this.toggle = function() {
+        this.selected = !this.selected;
+      };
+    },
+    controllerAs: 'tab',
     // template: `
     //   <div class="tabs__content" ng-if="tab.selected">
     //     <div ng-transclude></div>
     //   </div>
     // `,
-    link: function ($scope, $element, $attrs) {
+    link: function ($scope, $element, $attrs, $ctrl) {
       $scope.thing = "fred";
+      // $ctrl.al();
     }
   };
 }
