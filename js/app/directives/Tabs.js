69b6346fbb37a92588e0c8b226bf808364043217
agdavid <antonio.g.david@gmail.com>
2016-09-01 16:55:24 -0400

    create base of Tabs directive

--- js/app/directives/Tabs.js
+++ js/app/directives/Tabs.js
@@ -1,7 +1,22 @@
 angular
   .module('app')
-  .directive('tabs', Tabs)
+  .directive('tabs', tabs)
 
-function Tabs() {
-
+function tabs() {
+  return {
+    restrict: 'E', //restrict to use as element
+    scope: {}, //isolated scope
+    transclude: true, //indicate that will have insertion at ng-transclude
+    controller: function() {
+      this.tabs = [];
+    },
+    // 'tabs_list' will hold the values of each tab.label
+    // 'tabs_content' with ng-transclude will hold the tab content
+    template: `
+    <div class="tabs">
+      <ul class="tabs_list"></ul>
+      <div class="tabs_content" ng-transclude></div>
+     </div> 
+    `
+  };
 };
