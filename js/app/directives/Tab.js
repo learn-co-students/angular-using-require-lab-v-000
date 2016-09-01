27348330b7bef4a0207fba7cdab5344478b8d0eb
agdavid <antonio.g.david@gmail.com>
2016-09-01 16:49:57 -0400

    document coding logic; comment on directives in view

--- js/app/directives/Tab.js
+++ js/app/directives/Tab.js
@@ -3,25 +3,5 @@ angular
   .directive('tab', Tab)
 
 function Tab() {
-  return {
-    restrict: 'E',
-    scope: {
-      label: '@'
-    },
-    require: '^tabs', //require tabs directive, gain access to its controller
-    transclude: true,
-    template:
-    `
-      <div class="tabs__content" ng-if="tab.selected">
-        <div ng-transclude></div>
-      </div>
-    `,
-    link: function($scope, $element, $attrs, $ctrl) {
-      $scope.tab = {
-        label: $scope.label,
-        selected: false
-      };
-      $ctrl.addTab($scope.tab);
-    }
-  };
+
 };
