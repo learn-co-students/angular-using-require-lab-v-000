ed263bcd315e219d51ba98c7dcc098a2219676aa
agdavid <antonio.g.david@gmail.com>
2016-09-01 17:29:30 -0400

    Done.

--- js/app/directives/Tab.js
+++ js/app/directives/Tab.js
@@ -4,24 +4,54 @@ angular
 
 function Tab() {
   return {
-    restrict: 'E', //restrict to use as element
+    restrict: 'E',
     scope: {
-      label: '@' //in each tab directive, look for 'label' attr and use string literal value
+      label: '@'
     },
-    require: '^tabs', //communicate and get access to tabs directive and controller
-    transclude: true, //indicate that will have insertion at ng-transclude
-    // 'tabs_content' with ng-transclude will hold the tab content
-    template: `
-    <div class="tabs_content" ng-if="tab.selected">
-      <div ng-transclude></div>
-    </div>
+    require: '^tabs', //require tabs directive, gain access to its controller
+    transclude: true,
+    template:
+    `
+      <div class="tabs__content" ng-if="tab.selected">
+        <div ng-transclude></div>
+      </div>
     `,
     link: function($scope, $element, $attrs, $ctrl) {
-      $scope.tab { //in scope of each tab, set properties
-        label: $scope.label, //set label equal to value of the label attr
-        selected: false //set initial value of tab.selected to false
+      $scope.tab = {
+        label: $scope.label,
+        selected: false
       };
-      $ctrl.addTab($scope.tab); //each time tab directive is used, push into tabs array (3x)
+      $ctrl.addTab($scope.tab);
     }
   };
 };
+
+
+// angular
+//   .module('app')
+//   .directive('tab', Tab)
+
+// function Tab() {
+//   return {
+//     restrict: 'E', //restrict to use as element
+//     scope: {
+//       label: '@' //in each tab directive, look for 'label' attr and use string literal value
+//     },
+//     require: '^tabs', //communicate and get access to tabs directive and controller
+//     transclude: true, //indicate that will have insertion at ng-transclude
+//     // 'tabs_content' with ng-transclude will hold the tab content
+//     template:
+//     `
+//       <div class="tabs__content" ng-if="tab.selected">
+//         <div ng-transclude></div>
+//       </div>
+//     `,
+//     link: function($scope, $element, $attrs, $ctrl) {
+//       $scope.tab { //in scope of each tab, set properties
+//         label: $scope.label, //set label equal to value of the label attr
+//         selected: false //set initial value of tab.selected to false
+//       };
+//       $ctrl.addTab($scope.tab); //each time tab directive is used, push into tabs array (3x)
+//     }
+//   };
+// };
--- js/app/directives/Tabs.js
+++ js/app/directives/Tabs.js
@@ -1,43 +1,79 @@
 angular
   .module('app')
-  .directive('tabs', tabs)
+  .directive('tabs', Tabs)
 
-function tabs() {
+function Tabs() {
   return {
-    restrict: 'E', //restrict to use as element
-    scope: {}, //isolated scope
-    transclude: true, //indicate that will have insertion at ng-transclude
+    restrict: 'E', //restrict use of directive as element
+    scope: {}, //set isolated scope
+    transclude: true, //set as wrapper to recieve another directive
     controller: function() {
-      this.tabs = [];
-      //dynamically add each tab to parent directive 'tabs'
-      this.addTab = function(tab) {
+      this.tabs = []; //array to hold tabs
+      this.addTab = function(tab) { //function to add tabs to array
         this.tabs.push(tab);
       };
-      //use array to add clickable behavior to select correct tab on click
-      this.selectTab = function selectTab(index) {
-        for (var i; i < this.tabs.length; i++) {
+      this.selectTab = function(index) {
+        for(var i = 0; i < this.tabs.length; i++) {
           this.tabs[i].selected = false;
         }
         this.tabs[index].selected = true;
       };
     },
     controllerAs: 'tabs',
-    // set the active tab or first tab
     link: function($scope, $element, $attrs, $ctrl) {
       $ctrl.selectTab($attrs.active || 0);
     },
-    // 'tabs_list' will hold the values of each tab.label
-    // 'tabs_content' with ng-transclude will hold the tab content
-    // add 'selectTab' to tabs controller to specify tab's index and reveal content
-    template: `
-    <div class="tabs">
-      <ul class="tabs_list">
-        <li ng-repeat="tab in tabs.tabs">
-          <a href="" ng-bind="tab.label" ng-click="tabs.selectTab($index);"></a>
-        </li>
-      </ul>
-      <div class="tabs_content" ng-transclude></div>
-     </div> 
-    `
+    template: [
+      '<div class="tabs">',
+        '<ul class="tabs__list">',
+          '<li ng-repeat="tab in tabs.tabs">',
+          '<a href="" ng-bind="tab.label" ng-click="tabs.selectTab($index);"></a>',
+        '</ul>',
+      '<div class="tabs__content" ng-transclude></div>',
+      '</div>' 
+    ].join('')
   };
 };
+
+// angular
+//   .module('app')
+//   .directive('tabs', tabs)
+
+// function tabs() {
+//   return {
+//     restrict: 'E', //restrict to use as element
+//     scope: {}, //isolated scope
+//     transclude: true, //indicate that will have insertion at ng-transclude
+//     controller: function() {
+//       this.tabs = [];
+//       //dynamically add each tab to parent directive 'tabs'
+//       this.addTab = function(tab) {
+//         this.tabs.push(tab);
+//       };
+//       //use array to add clickable behavior to select correct tab on click
+//       this.selectTab = function(index) {
+//         for (var i = 0; i < this.tabs.length; i++) {
+//           this.tabs[i].selected = false;
+//         }
+//         this.tabs[index].selected = true;
+//       };
+//     },
+//     controllerAs: 'tabs',
+//     // set the active tab or first tab
+//     link: function($scope, $element, $attrs, $ctrl) {
+//       $ctrl.selectTab($attrs.active || 0);
+//     },
+//     // 'tabs_list' will hold the values of each tab.label
+//     // 'tabs_content' with ng-transclude will hold the tab content
+//     // add 'selectTab' to tabs controller to specify tab's index and reveal content
+//     template: [
+//         '<div class="tabs">',
+//           '<ul class="tabs__list">',
+//             '<li ng-repeat="tab in tabs.tabs">',
+//             '<a href="" ng-bind="tab.label" ng-click="tabs.selectTab($index);"></a>',
+//           '</ul>',
+//         '<div class="tabs__content" ng-transclude></div>',
+//         '</div>' 
+//     ].join('')
+//   };
+// };
