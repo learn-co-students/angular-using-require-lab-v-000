3f4f226e3fe7c2238f0976a0337436c14eef4497
agdavid <antonio.g.david@gmail.com>
2016-09-01 17:04:14 -0400

    create base tab directive with access to tabs controller

--- js/app/directives/Tab.js
+++ js/app/directives/Tab.js
@@ -3,5 +3,21 @@ angular
   .directive('tab', Tab)
 
 function Tab() {
+  return {
+    restrict: 'E', //restrict to use as element
+    scope: {
+      label: '@' //in each tab directive, look for 'label' attr and use string literal value
+    },
+    require: '^tabs', //communicate and get access to tabs directive and controller
+    transclude: true, //indicate that will have insertion at ng-transclude
+    // 'tabs_content' with ng-transclude will hold the tab content
+    template: `
+    <div class="tabs_content" ng-if="tab.selected">
+      <div ng-transclude></div>
+    </div>
+    `,
+    link: function($scope, $element, $attrs, $ctrl) {
 
+    }
+  };
 };
