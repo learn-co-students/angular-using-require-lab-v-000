5ed8f6eabe1f36c6b46431c055a2a9abd8847856
KerryAlsace <kerrydougherty@gmail.com>
2016-11-14 09:51:39 -0500

    Done.

--- js/app/directives/Tabs.js
+++ js/app/directives/Tabs.js
@@ -4,11 +4,25 @@ function Tabs() {
     scope: {},
     transclude: true,
     controller: function {
-
+      this.tabs = [];
+      this.addTab = function addTab(tab) {
+        this.tabs.push(tab);
+      };
+      this.selectTab = function selectTab(tabIndex) {
+        for (var i = 0; i < this.tabs.length; i++) {
+          this.tabs[i].selected = false;
+        }
+        this.tabs[tabIndex].selected = true;
+      };
     },
     controllerAs: 'tabs',
-    link: ,
-    template: 
+    link: function($scope, $element, $attrs, $ctrl) {
+      $ctrl.selectTab($attrs.active || 0); 
+    },
+    template: [
+      '<div>',
+      '</div>'
+    ].join('')
   };
 }
 
