439981f9c06dd6ee023474342f5d0c0afe35c340
KerryAlsace <kerrydougherty@gmail.com>
2016-11-14 10:06:47 -0500

    Done.

--- js/app/directives/Tab.js
+++ js/app/directives/Tab.js
@@ -1,14 +1,24 @@
 function Tab() {
   return {
     restrict: 'E',
-    scope: {},
-    transclude: true,
-    controller: function {
-
+    scope: {
+      label: '@'
     },
-    controllerAs: 'tabs',
-    link: ,
-    template: 
+    require: '^tabs',
+    transclude: true,
+    // controllerAs: 'tabs',
+    link: function($scope, $element, $attrs, $ctrl) {
+      $scope.tab = {
+        label: $scope.label,
+        selected: false
+      };
+      $ctrl.addTab($scope.tab);
+    },
+    template: [
+      '<div class="tabs__content" ng-if="tab.selected">',
+        '<div ng-transclude></div>',
+      '</div>'
+    ].join('')
   };
 }
 
--- js/app/directives/Tabs.js
+++ js/app/directives/Tabs.js
@@ -3,25 +3,31 @@ function Tabs() {
     restrict: 'E',
     scope: {},
     transclude: true,
-    controller: function {
+    controller: function () {
       this.tabs = [];
       this.addTab = function addTab(tab) {
         this.tabs.push(tab);
       };
-      this.selectTab = function selectTab(tabIndex) {
+      this.selectTab = function selectTab(index) {
         for (var i = 0; i < this.tabs.length; i++) {
           this.tabs[i].selected = false;
         }
-        this.tabs[tabIndex].selected = true;
+        this.tabs[index].selected = true;
       };
     },
     controllerAs: 'tabs',
-    link: function($scope, $element, $attrs, $ctrl) {
-      $ctrl.selectTab($attrs.active || 0); 
+    link: function ($scope, $element, $attrs, $ctrl) {
+      $ctrl.selectTab($attrs.active || 0);
     },
     template: [
-      '<div>',
-      '</div>'
+        '<div class="tabs">',
+          '<ul class="tabs__list">',
+                '<li ng-repeat="tab in tabs.tabs">',
+                  '<a href="" ng-bind="tab.label" ng-click="tabs.selectTab($index);"></a>',
+                '</li>',
+            '</ul>',
+          '<div class="tabs__content" ng-transclude></div>',
+        '</div>'
     ].join('')
   };
 }
