7a9529fffe8337f7736db30023b9c1aaaffdcf09
BrunoBoehm <bruno.boehm@gmail.com>
2017-02-28 18:41:40 +0100

    Done.

---
+++ js/app/directives/Tab.js
@@ -0,0 +1,29 @@
+function Tab() {
+	return {
+		restrict: 'E',
+		scope: {
+			label: '@'
+			// from the view <tab label="Tab 1">
+		},
+		require: '^tabs',
+		transclude: true,
+		template: [
+    	    '<div class="tabs__content" ng-if="tab.selected">',
+      	        '<div ng-transclude></div>',
+            '</div>'
+            // will go inside of <tab label="Tab 1">
+		].join(''),
+		link: function ($scope, $element, $attrs, $ctrl) {
+			$scope.tab = {
+				label: $scope.label,
+				selected: false
+			};
+			$ctrl.addTab($scope.tab);
+			// $ctrl is the 4th argument, given because we use 'require'
+		}
+	}
+}
+
+angular
+	.module('app')
+	.directive('tab', Tab);
